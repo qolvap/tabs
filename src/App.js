c78f05086ceee9a7039c096958ef4599ac15ca94
qolvap <pgrebennikow@yahoo.pl>
2023-11-06 22:27:04 +0100

    ✅ logic & styles updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
+import { data } from './data.js';
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [jobs, setJobs] = useState(data);
+  const [value, setValue] = useState(0);
+
+  if (loading) {
+    return (
+      <section className='section loading'>
+        <h1>loading...loading..loading...</h1>
+      </section>
+    );
+  }
+
+  const { company, dates, duties, title } = jobs[value];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='section'>
+      <div className='title'>
+        <h2>experience</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='jobs-center'>
+        <div className='btn-container'>
+          {jobs.map((item, index) => (
+            <button
+              key={item.id}
+              onClick={() => setValue(index)}
+              className={`job-btn ${index === value && 'active-btn'}`}
+            >
+              {item.title}
+            </button>
+          ))}
+        </div>
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty, index) => (
+            <div key={index} className='job-desc'>
+              <FaAngleDoubleRight className='job-icon' />
+              <p>{duty}</p>
+            </div>
+          ))}
+        </article>
+      </div>
+      <button type="button" className="btn">
+      <a href="https://www.linkedin.com/in/qolvap/" target="_blank" rel="noopener noreferrer">
+    More Info
+  </a>
+      </button>
+    </section>
   );
-}
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,35 @@
+export const data = [
+    {
+      id: 'rec1',
+      order: 1,
+      title: 'Front End Developer',
+      dates: '2023 - present',
+      duties: ['Working on building web pages & applications for non-profit organisations as part of the remote front-end team.' ,
+                'In cooperation with back-end team & testers, under the eyes of PO & PM.',
+                'Tech stack: JavaSccript, TypeScript, ES6, Tailwind, Styled Components, SCC, HTML5',
+                'Tools: React.js, Git, Chat GPT'
+                ],
+      company: 'Baza Trainee'
+    },
+    {
+      id: 'rec2',
+      order: 2,
+      title: 'Project Manager',
+      dates: '2021 - present',
+      duties: ['Team leading, trainings, motivation', 'Task prioritizations, resource planning', 'Direct contact with the client & business partner', 
+                'Social media management & marketing strategy'
+                ],
+      company: 'Tacho Driver'
+    },
+    {
+      id: 'rec3',
+      order: 3,
+      title: 'Transport Law Expert',
+      dates: '2021 - present',
+      duties: ['Provide advice in Mobility Package Law, Posting Declaration Portal, contributing to relevant training', 
+                'Monitor driver compliance & identify driver hours infringements', 'Investigate the cause of infringements, take effective action to address any cases identiﬁed', 
+                'Keep proper records of the entire process'
+                ],
+      company: 'Tacho Driver'
+    },
+  ];
